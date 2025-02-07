dc583a00a7f0a7b26c4765d3bdfa49aefad3caf8
mhelve46 <mhelve46@students.byron.k12.mn.us>
2025-02-07 17:34:14 -0600

    huge code dump, because it would be a pain to PR to main or Prototype

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -170,6 +170,13 @@ public class Constants {
     public static final class ClawConstants {
     }
 
+    public static final class VisionConstants {
+        public static final String limeLightName = "limelight";
+        public static final String limeLightName2 = "";
+        public static final int aprilPipe = 0;
+        // public static final int nPipe = 1;
+    }
+
     public static final class PoseSetter {{
         if (Selector.PlacementSelector.getLevel() == Selector.PlacementSelector.L1){
             // elevatorStage1Target = 'stage 1 = 0'
